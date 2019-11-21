bbdb4005c48f1817938e15ff262e41a4b757787e
Jeff Selby <selbyjef1593@gmail.com>
2019-11-21 12:57:03 -0500

    Delete motorStop.ino

--- motorStop.ino
+++
@@ -1,7 +0,0 @@
-//and this code will stop both motors
-void motorStop() {
-  digitalWrite(motor1a, LOW);
-  digitalWrite(motor1b, LOW);
-  digitalWrite(motor2a, LOW);
-  digitalWrite(motor2b, LOW);
-}
